1abcbddd6a55b11d35b41afc2768818804caa9f4
Quinlan Hill <quinhill@icloud.com>
2018-08-22 07:28:54 -0600

    Add styling

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -4,23 +4,29 @@ import './contact.css';
 const Contact = () => {
   return (
     <div>
-      <p className="contact-info">
-        2402 S Corona St, 80210 Denver, CO
-      </p>
-      <span>phone
+      <div className="address">
+        <p className="contact-info">Address:</p>
+        <p className="contact-info">
+          2402 S Corona St, 80210 Denver, CO
+        </p>
+      </div>
+      <span>Phone:
         <a 
-          className="phone" 
+          className="link" 
           href="tel:+17208094383"
         >
           (720) 809-4383
         </a>
       </span>
-      <a 
-        className="email"
-        href="mailto:quinlan@quinlanhill.com"
-      >
-        quinlan@quinlanhill.com
-      </a>
+      <span>
+        Email:
+        <a 
+          className="link"
+          href="mailto:quinlan@quinlanhill.com"
+        >
+          quinlan@quinlanhill.com
+        </a>
+      </span>
     </div>
   )
 }
--- src/Resume/index.js
+++ src/Resume/index.js
@@ -4,7 +4,23 @@ import './resume.css';
 const Resume = () => {
   return (
     <div className="resume">
-
+      <img className="resume-img" src="/quin-resume.jpg" />
+      <p className="download-resume">
+        Download résumé:
+        <a 
+          href="/quinlan-hill-resume-color.pdf" 
+          download
+        >
+          color
+        </a>
+        |
+        <a 
+          href="/quinlan-hill-resume-grayscale.pdf" 
+          download
+        >
+          printer friendly (grayscale)
+        </a>
+      </p>
     </div>
   )
 }
