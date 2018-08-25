2ee00b75379921e6d0b7108271248988832cb1b3
Quinlan Hill <quinhill@icloud.com>
2018-08-25 12:51:31 -0600

    Remove Skills and Contact components

--- src/Main/index.js
+++ src/Main/index.js
@@ -3,8 +3,6 @@ import './main.css';
 import Bio from '../Bio/Bio';
 import Projects from '../Projects/Projects';
 import Resume from '../Resume';
-import Skills from '../Skills';
-import Contact from '../Contact';
 import BioGer from '../Bio/BioGer';
 import ProjectsGer from '../Projects/ProjectsGer';
 
@@ -19,13 +17,9 @@ const Main = ({props}) => {
     display = <Projects />
   } else if (props.display === 'projects' && !props.english) {
     display = <ProjectsGer />
-  } else if (props.display === 'skills') {
-    display = <Skills />
   } else if (props.display === 'resume') {
     display = <Resume />
-  } else if (props.display === 'contact') {
-    display = <Contact />
-  } 
+  }
 
   return (
     <div className="main">
